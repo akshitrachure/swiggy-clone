9725e0c99ab4faa65811747fe7440bfaac2796cb
Akshit Rachure <rachureakshit1899@gmail.com>
2023-07-06 16:00:56 +0530

    Restaurant Menu page changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -36,9 +36,9 @@ const RestaurantMenu=()=>{
             {/* <h1>Restaurant ID:{restaurant.id}</h1> */}
             <h1 className="font-bold text-3xl py-3 text-center">{restaurantApi?.cards[0]?.card?.card?.info?.name}</h1>
              <img className="w-96 py-2" src={IMG_CDN_URL+restaurantApi?.cards[0]?.card?.card?.info?.cloudinaryImageId} />
-            <h3 className="px-4 font-semibold text-lg">Area: {restaurantApi?.data?.cards[0]?.card?.card?.info?.areaName}</h3>
-            <h3 className="px-4 font-semibold text-lg">City: {restaurantApi?.data?.cards[0]?.card?.card?.info?.city}</h3>
-            <h3 className="px-4 font-semibold text-lg">Rating: {restaurantApi?.data?.cards[0]?.card?.card?.info?.avgRating} stars</h3>
+            <h3 className="px-4 font-semibold text-lg">Area: {restaurantApi?.cards[0]?.card?.card?.info?.areaName}</h3>
+            <h3 className="px-4 font-semibold text-lg">City: {restaurantApi?.cards[0]?.card?.card?.info?.city}</h3>
+            <h3 className="px-4 font-semibold text-lg">Rating: {restaurantApi?.cards[0]?.card?.card?.info?.avgRating} stars</h3>
             <h3 className="px-4 font-semibold text-lg">{restaurantApi?.cards[0]?.card?.card?.info?.costForTwo/100+"/- for two"}</h3>
         </div>
         <div className="px-10 py-3">
