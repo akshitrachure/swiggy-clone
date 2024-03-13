80f346a9877acbb14bb8c13c4e37f79d48591ecd
Akshit Rachure <rachureakshit1899@gmail.com>
2024-03-13 13:59:27 +0530

    ChangedRestaurantsLink

--- src/utils/useRestaurantDetails.js
+++ src/utils/useRestaurantDetails.js
@@ -1,26 +1,25 @@
 import { useState, useEffect } from "react";
 import { FETCH_RESTAURANT_DETAILS_URL } from "../config";
 
-const useRestaurantDetails=()=>{
-    
-    const [restaurant, setRestaurant] = useState([]);
+const useRestaurantDetails = () => {
+  const [restaurant, setRestaurant] = useState([]);
 
-    useEffect(()=>{
-        getRestaurantDetails();
-    },[]);
+  useEffect(() => {
+    getRestaurantDetails();
+  }, []);
 
-
-    async function getRestaurantDetails(){
-        //console.log("Fetchingg details");
-        const data = await fetch(FETCH_RESTAURANT_DETAILS_URL);
-      //  console.log("Fetched details");
-        const json =  await data.json();
-       // console.log(json);
-        setRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-       // console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    }
-    return restaurant;
-}
-
+  async function getRestaurantDetails() {
+    //console.log("Fetchingg details");
+    const data = await fetch(FETCH_RESTAURANT_DETAILS_URL);
+    //  console.log("Fetched details");
+    const json = await data.json();
+    console.log(json);
+    setRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    // console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+  return restaurant;
+};
 
 export default useRestaurantDetails;
